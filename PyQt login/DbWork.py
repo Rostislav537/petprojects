e136cdc9cf25373b3d7f66f7df7798152c3d12a4
Rostislav537 <140638708+Rostislav537@users.noreply.github.com>
2024-06-18 16:15:56 +0300

    Update DbWork.py

--- PyQt login/DbWork.py
+++ PyQt login/DbWork.py
@@ -6,6 +6,7 @@ class DbWork:
         self.cursor = self.connection.cursor()
 
         self.cursor.execute('CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY,username TEXT NOT NULL,password TEXT NOT NULL)')
+        
     def AddUser(self, username, password):  #добавлюет нового юзера
         if len(password)<8:     #проверка длинны пароля
             return "Failed, password so short"
@@ -21,6 +22,7 @@ class DbWork:
         self.cursor.execute('INSERT INTO Users (username, password) VALUES (?, ?)',(username, password))
         self.connection.commit()
         return "Success"
+        
     def CheckUser(self, username, password):    #проверка пароля
         try:    #трай ексепт так как если пользователь не находится - выдает ошибку
             self.cursor.execute('SELECT username, password FROM Users WHERE username = ?', (username,))
@@ -32,6 +34,7 @@ class DbWork:
                 return False
         except:
             return "Error, this username not in database"
+            
     def DeleteUser(self, username, password):   #удаление юзера с базы данных
         self.cursor.execute('SELECT * FROM Users')
         results = self.cursor.fetchall()
@@ -41,9 +44,11 @@ class DbWork:
                 self.connection.commit()
                 return "Success"
         return "Failed, data is incorrect"
+        
     def ViewBase(self):     #возвращает список всех юзеров
         self.cursor.execute('SELECT username, id FROM Users')
         results = self.cursor.fetchall()
         return results
+        
     def CloseDB(self):  #закрывает базу данных
         self.connection.close()
